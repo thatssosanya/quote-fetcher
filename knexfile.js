c8c4eec4bf33cc62f2f80d72be8239f39399103d
stonekite <feelsknight@gmail.com>
2023-01-25 16:42:26 +0300

    dropped mssql support

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const debugConf = {
   client: 'sqlite3',
   connection: {
@@ -6,12 +9,4 @@ export const debugConf = {
   useNullAsDefault: true
 };
 
-export const cronConf = {
-  client: 'mssql',
-  connection: {
-    server: process.env.MSSQL_SERVER,
-    database: process.env.MSSQL_DATABASE,
-    user: process.env.MSSQL_USERNAME,
-    password: process.env.MSSQL_PASSWORD
-  }
-};
+export const cronConf = debugConf;
